e84c874530027be3ee3818ab2011b024497adf7b
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-09 20:38:56 -0300

    Criando operacao de instanciacao

--- src/main/java/com/fredericoahb/cursomc/CursomcApplication.java
+++ src/main/java/com/fredericoahb/cursomc/CursomcApplication.java
@@ -1,13 +1,37 @@
 package com.fredericoahb.cursomc;
 
+import java.util.Arrays;
+
+import org.hibernate.mapping.Array;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fredericoahb.cursomc.domain.Categoria;
+import com.fredericoahb.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	//dependência da camada Repository para a criação dos objetos no BD
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		//salva as categorias no banco
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
