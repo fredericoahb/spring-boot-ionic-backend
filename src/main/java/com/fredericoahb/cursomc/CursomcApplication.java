73876e12a5498015baeaa1463ed85c5b55131979
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-10 19:25:37 -0300

    Commit: Estado e Cidade

--- src/main/java/com/fredericoahb/cursomc/CursomcApplication.java
+++ src/main/java/com/fredericoahb/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.fredericoahb.cursomc.domain.Categoria;
+import com.fredericoahb.cursomc.domain.Cidade;
+import com.fredericoahb.cursomc.domain.Estado;
 import com.fredericoahb.cursomc.domain.Produto;
 import com.fredericoahb.cursomc.repositories.CategoriaRepository;
+import com.fredericoahb.cursomc.repositories.CidadeRepository;
+import com.fredericoahb.cursomc.repositories.EstadoRepository;
 import com.fredericoahb.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -22,6 +26,11 @@ public class CursomcApplication implements CommandLineRunner {
 	//criando dependencia de produto da camada Repository
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -49,9 +58,21 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		//salva as categorias no banco
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 
---
+++ src/main/java/com/fredericoahb/cursomc/domain/Cidade.java
@@ -0,0 +1,84 @@
+package com.fredericoahb.cursomc.domain;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cidade implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	//no atributo associado fazer o mapeamento das entidades
+	@ManyToOne
+	@JoinColumn(name = "estado_id")
+	//Associação muitos para um. Uma cidade em um Estado
+	private Estado estado;
+
+	//construtores
+	public Cidade() {
+		}
+
+	public Cidade(Integer id, String nome, Estado estado) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cidade other = (Cidade) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+		
+}
