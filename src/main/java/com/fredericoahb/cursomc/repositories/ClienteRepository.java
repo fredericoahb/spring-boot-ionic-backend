0dbf28964d078687f66b98fc5629ab38c308eaae
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-11 14:22:05 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/fredericoahb/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/fredericoahb/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.fredericoahb.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fredericoahb.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 
 }
--- src/main/java/com/fredericoahb/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/fredericoahb/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,11 +6,18 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fredericoahb.cursomc.domain.Cliente;
 import com.fredericoahb.cursomc.domain.enums.TipoCliente;
 import com.fredericoahb.cursomc.dto.ClienteNewDTO;
+import com.fredericoahb.cursomc.repositories.ClienteRepository;
 import com.fredericoahb.cursomc.resources.exception.FieldMessage;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
 
 	@Override
 	public void initialize(ClienteInsert ann) {
@@ -29,6 +36,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
 
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
