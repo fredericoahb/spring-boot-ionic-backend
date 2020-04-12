251d25b70287c81a634fe10895b380a65ab5f2db
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-12 13:16:50 -0300

    Adicionando senha a Cliente

--- src/main/java/com/fredericoahb/cursomc/domain/Cliente.java
+++ src/main/java/com/fredericoahb/cursomc/domain/Cliente.java
@@ -33,6 +33,9 @@ public class Cliente implements Serializable {
 		private String cpfOuCnpj;
 		private Integer tipo;
 		
+		@JsonIgnore
+		private String senha;
+		
 		//resolvendo o problema da referencia ciclica. Cliente pode serializar seu endereco
 
 		@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
@@ -53,13 +56,14 @@ public class Cliente implements Serializable {
 		public Cliente() {
 		}
 
-		public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo) {
+		public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
 			super();
 			this.id = id;
 			this.nome = nome;
 			this.email = email;
 			this.cpfOuCnpj = cpfOuCnpj;
 			this.tipo = (tipo==null) ? null : tipo.getCod();
+			this.senha = senha;
 		}
 
 		public Integer getId() {
@@ -101,6 +105,14 @@ public class Cliente implements Serializable {
 
 		public void setTipo(TipoCliente tipo) {
 			this.tipo = tipo.getCod();
+		}
+		
+		public String getSenha() {
+			return senha;
+		}
+
+		public void setSenha(String senha) {
+			this.senha = senha;
 		}
 
 		public List<Endereco> getEnderecos() {
