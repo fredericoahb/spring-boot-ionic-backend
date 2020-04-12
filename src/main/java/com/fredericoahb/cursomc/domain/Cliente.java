016b660d0f89c432df5d57a19e05170d79bd997a
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-12 13:25:12 -0300

    Salvando perfis de usuario na base de dados

--- src/main/java/com/fredericoahb/cursomc/domain/Cliente.java
+++ src/main/java/com/fredericoahb/cursomc/domain/Cliente.java
@@ -5,18 +5,21 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fredericoahb.cursomc.domain.enums.Perfil;
 import com.fredericoahb.cursomc.domain.enums.TipoCliente;
 
 @Entity
@@ -47,6 +50,10 @@ public class Cliente implements Serializable {
 		@CollectionTable(name="TELEFONE")
 		private Set<String> telefones = new HashSet<>();
 		
+		@ElementCollection(fetch=FetchType.EAGER)
+		@CollectionTable(name="PERFIS")
+		private Set<Integer> perfis = new HashSet<>();
+		
 		//nao é permitido pedido serializar cliente
 		@JsonIgnore
 		//Cliente tem que conhecer o pedido. Relacionamento birecional. Como é uma coleção, não precisa de construtor
@@ -54,6 +61,7 @@ public class Cliente implements Serializable {
 		private List<Pedido> pedidos = new ArrayList<>();
 
 		public Cliente() {
+			addPerfil(Perfil.CLIENTE);
 		}
 
 		public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -64,6 +72,7 @@ public class Cliente implements Serializable {
 			this.cpfOuCnpj = cpfOuCnpj;
 			this.tipo = (tipo==null) ? null : tipo.getCod();
 			this.senha = senha;
+			addPerfil(Perfil.CLIENTE);
 		}
 
 		public Integer getId() {
@@ -113,6 +122,14 @@ public class Cliente implements Serializable {
 
 		public void setSenha(String senha) {
 			this.senha = senha;
+		}
+		
+		public Set<Perfil> getPerfis() {
+			return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+		}
+
+		public void addPerfil(Perfil perfil) {
+			perfis.add(perfil.getCod());
 		}
 
 		public List<Endereco> getEnderecos() {
