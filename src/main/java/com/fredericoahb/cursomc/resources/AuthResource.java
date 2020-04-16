cb6fae464eec2f6aaedbbce005766a98fb7d1749
Frederico Balieiro <fredericoahb@gmail.com>
2020-04-15 21:33:22 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/fredericoahb/cursomc/resources/AuthResource.java
+++ src/main/java/com/fredericoahb/cursomc/resources/AuthResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fredericoahb.cursomc.dto.EmailDTO;
 import com.fredericoahb.cursomc.security.JWTUtil;
 import com.fredericoahb.cursomc.security.UserSS;
 import com.fredericoahb.cursomc.services.AuthService;
